00c31e180879ae9e461f62ba495c4578183c5eaf
BryanPinarAmper <47842329+Cobybryanz@users.noreply.github.com>
2022-10-31 16:17:06 +0800

    Create first_in_first_out.c

---
+++ first_in_first_out.c
@@ -0,0 +1,45 @@
+// FIFO Code
+int led1 = 7;
+int led2 = 6;
+int led3 = 5;
+int led4 = 4;
+int led5 = 3;
+int led6 = 2;
+
+// Led Output
+void setup()
+{
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+ 
+}
+
+// Process Code
+void loop()
+
+{
+  digitalWrite(led1, HIGH);
+  delay(10000); // Delay time for 10 second(s)
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH);
+  delay(5000); // Delay time for 5 second(s)
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, HIGH);
+  delay(8000); // Delay time for 8 second(s)
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, HIGH);
+  delay(3000); // Delay time for 3 second(s)
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, HIGH);
+  delay(1000); // Delay time for 1 second(s)
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, HIGH);
+  delay(2000); // Delay time for 2 second(s)
+  digitalWrite(led6, LOW);
+
+}
